2f8099797351a9b9cd35bcd9a32743e7b6525243
Alvinn8 <42838560+Alvinn8@users.noreply.github.com>
2024-02-12 13:14:15 +0100

    PoolAllocator allocation

--- main.cpp
+++ main.cpp
@@ -1,12 +1,13 @@
 #include <iostream>
+#include <vector>
+#include <cmath>
 
 class Allocator {
 public:
     virtual void* alloc(size_t size, size_t alignment) = 0;
-    virtual void  free(void* ptr) = 0;
+    virtual void  free(void* ptr, size_t size) = 0;
     virtual void* resize(void* ptr, size_t oldSize, size_t newSize) = 0;
 
-    // Virtual destructor to ensure proper destruction of derived classes
     virtual ~Allocator() {}
 };
 
@@ -50,7 +51,7 @@ public:
         return reinterpret_cast<void*>(addr);
     }
 
-    virtual void free(void* ptr) override {
+    virtual void free(void* ptr, size_t size) override {
         ; // no-op
     }
 
@@ -69,13 +70,129 @@ public:
     }
 };
 
-int main() {
+
+class PoolAllocator : public Allocator {
+    private:
+        void* buffer;
+        size_t totalSize;
+        size_t blockSize;
+        std::vector<uint8_t> mask;
+
+public:
+    PoolAllocator(size_t blockSize, size_t blockCount) {
+        this->blockSize = blockSize;
+        totalSize = blockSize * blockCount;
+        buffer = std::malloc(totalSize);
+        std::cout << "buffer in constructor: " << reinterpret_cast<size_t>(buffer) << std::endl;
+        if (!buffer) {
+            throw std::bad_alloc();
+        }
+        size_t maskSize = std::ceil(blockCount / 8.0);
+        std::vector<uint8_t> mask(maskSize);
+        this->mask = mask;
+    }
+
+    ~PoolAllocator() {
+        std::cout << "addr when freeing: " << reinterpret_cast<size_t>(buffer) << std::endl;
+        std::free(buffer);
+    }
+    
+    virtual size_t nextBlock(size_t searchFrom, bool set) {
+        // Find next free block
+        for (size_t byteIndex = searchFrom / 8; byteIndex < mask.size(); byteIndex++) {
+            uint8_t byte = mask.at(byteIndex);
+            for (size_t bitIndex = searchFrom % 8; bitIndex < 8; bitIndex++) {
+                bool bit = (byte >> bitIndex) & 1;
+                if (bit == set) {
+                    return byteIndex * 8 + bitIndex;
+                }
+            }
+        }
+        return -1;
+    }
+
+    virtual size_t nextFreeBlock(size_t searchFrom) {
+        return nextBlock(searchFrom, false);
+    }
+
+    virtual size_t nextUsedBlock(size_t searchFrom) {
+        return nextBlock(searchFrom, true);
+    }
+
+    virtual size_t findBlock(size_t size) {
+        // Find next free block that fits our data
+        size_t blockCount = size / blockSize;
+        size_t searchFrom = 0;
+        while (true) {
+            size_t freeIndex = this->nextFreeBlock(searchFrom);
+            size_t usedIndex = this->nextFreeBlock(searchFrom);
+            if (freeIndex == -1) {
+                // Oops, we ran out of space.
+                return -1;
+            }
+            if (usedIndex == -1 || usedIndex - freeIndex >= blockCount) {
+                // This block fits our data!
+                return freeIndex;
+            }
+            searchFrom = usedIndex;
+        }
+    }
+
+    virtual size_t setBlockUsed(size_t blockIndex, bool used) {
+        // Find next free block
+        size_t byteIndex = blockIndex / 8;
+        size_t bitIndex = blockIndex % 8;
+        uint8_t byte = mask.at(byteIndex);
+        // Set the bit
+        byte = byte | (1 << bitIndex);
+        mask[byteIndex] = byte;
+    }
+
+    virtual void* alloc(size_t size, size_t alignment) override {
+        size_t blockIndex = this->findBlock(size);
+        std::cout << "block index: " << blockIndex << std::endl;
+        if (blockIndex == -1) {
+            return nullptr;
+        }
+        size_t sizeUsed = size;
+        size_t addr = reinterpret_cast<size_t>(buffer) + blockIndex * blockSize;
+        std::cout << "addr: " << addr << std::endl;
+        if (addr % alignment != 0) {
+            // Oh... it's not aligned? Well too bad, it's hard to know the
+            // alignment of the address before we have the address.
+            // Hopefully users of a PoolAllicator will make the block size
+            // aligned...
+            throw std::bad_alloc();
+        }
+        std::cout << "size: " << size << std::endl;
+        std::cout << "sizeUsed: " << sizeUsed << std::endl;
+        size_t blocksUsed = std::ceil(sizeUsed / (double) blockSize);
+        std::cout << "blocksUsed: " << blocksUsed << std::endl;
+        // Mark blocks as used
+        for (size_t i = blockIndex; i < blocksUsed; i++) {
+            setBlockUsed(i, true);
+        }
+        return reinterpret_cast<void*>(addr);
+    }
+
+    virtual void free(void* ptr, size_t size) override {
+        
+    }
+
+    virtual void* resize(void* ptr, size_t oldSize, size_t newSize) override {
+        
+    }
+};
+
+int test() {
     std::cout << "Hello World!";
 
-    LinearAllocator allocator(100);
+    // LinearAllocator allocator(100);
+    PoolAllocator allocator(32, 8);
 
     uint8_t* b1 = static_cast<uint8_t*>(allocator.alloc(1, 1));
-    int* array = static_cast<int*>(allocator.alloc(5 * sizeof(int), 4));
+    int arrayByteSize = 5 * sizeof(int);
+    int* array = static_cast<int*>(allocator.alloc(arrayByteSize, 4));
 
     allocator.resize(array, 5 * sizeof(int), 10 * sizeof(int));
 
@@ -101,8 +218,12 @@ int main() {
     }
     std::cout << std::endl;
 
-    allocator.free(b1);
-    allocator.free(array);
+    allocator.free(b1, 1);
+    allocator.free(array, arrayByteSize);
 
     return 0;
+}
+
+int main() {
+    test();
 }
